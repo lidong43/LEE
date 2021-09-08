9a6e2895bb339da0c9cc957274849c9a449ab083
lidong_admin <1737809323@qq.com>
2021-09-08 17:52:24 +0800

    9.24提交数组

--- git_test01/src/test01.java
+++ git_test01/src/test01.java
@@ -1,5 +1,11 @@
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        //数组创建
+        int[] arr=new int[]{1,2,3,4};
+        System.out.println(arr[0]);
+        //数组遍历
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
