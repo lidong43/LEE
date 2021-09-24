c84bf97d2f44c35eebe42da819bdd72c92de09e3
lidong_admin <1737809323@qq.com>
2021-09-24 19:45:20 +0800

    9.24提交数组2

--- git_test01/src/test01.java
+++ git_test01/src/test01.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
+
 public class test01 {
     public static void main(String[] args) {
         //数组创建
-        int[] arr=new int[]{1,2,3,4};
+        int[] arr=new int[]{1,5,3,4};
         System.out.println(arr[0]);
         //数组遍历
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+        //数组排序
+        Arrays.sort(arr);
+        for(int i:arr){
+            System.out.println(i);
+        }
+
     }
 }
